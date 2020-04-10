ed43c291e52742d9d4830bda51d2c755d83f4987
jhejna <joey.hejna@gmail.com>
2020-04-10 12:41:43 -0700

    update hyper params

--- ul_gen/configs/ppo_procgen_config.py
+++ ul_gen/configs/ppo_procgen_config.py
@@ -5,16 +5,17 @@ configs = dict()
 config = dict(
     agent=dict(),
     algo=dict(
-        discount=0.99,
+        discount=0.999,
         learning_rate=5e-4,
         value_loss_coeff=1.,
         entropy_loss_coeff=0.01,
         clip_grad_norm=1.,
-        gae_lambda=0.98,
+        gae_lambda=0.95,
         linear_lr_schedule=True,
         minibatches=8,
         epochs=3,
         ratio_clip=.2,
+        normalize_advantage=True
     ),
     env={
         "id": "procgen:procgen-coinrun-v0",
@@ -23,10 +24,10 @@ config = dict(
         "distribution_mode": "easy"
     },
     model=dict(
-        cnn_filters=[32, 64, 64],
+        cnn_filters=[16, 32, 32],
         cnn_output=256,
-        policy_layers=[64, 64, 15],
-        value_layers=[64, 64, 1]
+        policy_layers=[15],
+        value_layers=[1]
     ),
     optim=dict(),
     runner=dict(
@@ -34,7 +35,7 @@ config = dict(
         log_interval_steps=1e5,
     ),
     sampler=dict(
-        batch_T=64,
+        batch_T=256,
         batch_B=16,
         eval_n_envs=10,
         eval_max_steps=20000,
