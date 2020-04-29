665f3c20ae2abe292a76a69399a42c79e460cf6e
karamarieliu <karamarieliu@berkeley.edu>
2020-04-28 17:45:12 -0700

    commiting beforre pulling'

--- ul_gen/configs/ppo_vae_procgen_config.py
+++ ul_gen/configs/ppo_vae_procgen_config.py
@@ -17,20 +17,20 @@ config = dict(
         ratio_clip=.2,
         normalize_advantage=True,
         vae_beta=1,
-        vae_loss_coeff=0.1,
+        vae_loss_coeff=1e-3,
         vae_loss_type="l2",
     ),
     env={
         "id": "procgen:procgen-coinrun-v0",
-        "num_levels": 500,
+        "num_levels": 300,
         "start_level": 0,
-        "distribution_mode": "easy"
+        "distribution_mode": "hard"
     },
     eval_env={
         "id": "procgen:procgen-coinrun-v0",
-        "num_levels": 100,
+        "num_levels": 50,
         "start_level": 1000,
-        "distribution_mode": "easy"
+        "distribution_mode": "hard"
     },
     model=dict(
         zdim=200,
