1d00df605053219cf3e647c75168c4756944c886
jhejna <joey.hejna@gmail.com>
2020-05-04 19:37:42 -0700

    update aug vaes

--- ul_gen/aug_vae/vae.py
+++ ul_gen/aug_vae/vae.py
@@ -19,7 +19,7 @@ class PrintNode(torch.nn.Module):
 
 class VAE(torch.nn.Module):
 
-    def __init__(self, img_dim=64, img_channels=3, z_dim=32, final_act="tanh"):
+    def __init__(self, img_dim=64, img_channels=3, z_dim=32, final_act="tanh", fc_size=256):
         super(VAE, self).__init__()
         self.img_dim = img_dim
         self.z_dim = z_dim
@@ -28,45 +28,35 @@ class VAE(torch.nn.Module):
         final_act_fn = lambda: torch.nn.Tanh() if final_act == "tanh" else torch.nn.Sigmoid()
 
         final_feature_dim = img_dim // (2**4)
-        self.encoder_net = torch.nn.Sequential(torch.nn.Conv2d(self.img_channels, 32, kernel_size=4, stride=2, padding=1),
-                                            # PrintNode("Enc Conv1"),
-                                            torch.nn.ReLU(),
+        self.encoder_net = torch.nn.Sequential(
+                                            torch.nn.Conv2d(self.img_channels, 32, kernel_size=4, stride=2, padding=1),
+                                            torch.nn.ReLU(True),
                                             torch.nn.Conv2d(32, 32, kernel_size=4, stride=2, padding=1),
-                                            # PrintNode("Enc Conv2"),
-                                            torch.nn.ReLU(),
+                                            torch.nn.ReLU(True),
                                             torch.nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
-                                            # PrintNode("Enc Conv3"),
-                                            torch.nn.ReLU(),
+                                            torch.nn.ReLU(True),
                                             torch.nn.Conv2d(64, 64, kernel_size=4, stride=2, padding=1),
-                                            # PrintNode("Enc Conv4"),
-                                            torch.nn.ReLU(),
+                                            torch.nn.ReLU(True),
                                             torch.nn.Flatten(),
-                                            torch.nn.Linear(final_feature_dim*final_feature_dim*64, 256),
-                                            torch.nn.ReLU(),
-                                            # PrintNode("Flattened Enc"),
-                                            torch.nn.Linear(256, 2*self.z_dim),
-                                            # PrintNode("Enc Z Out"),
+                                            torch.nn.Linear(final_feature_dim*final_feature_dim*64, fc_size),
+                                            torch.nn.ReLU(True),
+                                            torch.nn.Linear(fc_size, 2*self.z_dim),
                                             )
-        
-        self.decoder_net = torch.nn.Sequential(torch.nn.Linear(self.z_dim, 256),
-                                            torch.nn.ReLU(),
-                                            torch.nn.Linear(256, final_feature_dim*final_feature_dim*64),
+        self.decoder_net = torch.nn.Sequential(torch.nn.Linear(self.z_dim, fc_size),
+                                            torch.nn.ReLU(True),
+                                            torch.nn.Linear(fc_size, final_feature_dim*final_feature_dim*64),
                                             Reshape((64, final_feature_dim, final_feature_dim)),
-                                            # PrintNode("Dec Conv In"),
+                                            torch.nn.ReLU(True),
                                             torch.nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1),
-                                            # PrintNode("Dec Conv1"),
-                                            torch.nn.ReLU(),
+                                            torch.nn.ReLU(True),
                                             torch.nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
-                                            # PrintNode("Dec Conv2"),
-                                            torch.nn.ReLU(),
+                                            torch.nn.ReLU(True),
                                             torch.nn.ConvTranspose2d(32, 32, kernel_size=4, stride=2, padding=1),
-                                            # PrintNode("Dec Conv3"),
-                                            torch.nn.ReLU(),
+                                            torch.nn.ReLU(True),
                                             torch.nn.ConvTranspose2d(32, self.img_channels, kernel_size=4, stride=2, padding=1),
-                                            # PrintNode("Dec Conv4"),
                                             final_act_fn(),
                                             )
-
+    
     def encoder(self, x):
         mu, log_var = torch.chunk(self.encoder_net(x), 2, dim=1)
         return mu, log_var
